7e5ac31388254d0c548253fa30ff83c9b9974345
jwf545-cyber <jwf545@york.ac.uk>
2026-03-25 10:24:22 +0000

    Add files via upload

---
+++ Coordinate.java
@@ -0,0 +1,60 @@
+
+public class Coordinates {
+
+    // Attributes (private for encapsulation)
+    private int x;
+    private int y;
+
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+
+    public int getY() {
+        return y;
+    }
+
+
+    public double distance(Coordinates p) {
+        int dx = this.x - p.getX();
+        int dy = this.y - p.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+
+    public void translate(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+
+    public void scale(int factor, boolean sign) {
+        if (sign) {
+            this.x *= factor;
+            this.y *= factor;
+        } else {
+
+            if (factor != 0) {
+                this.x /= factor;
+                this.y /= factor;
+            } else {
+                System.out.println("Error: factor cannot be zero.");
+            }
+        }
+    }
+
+    /**
+     * Display coordinates as a string
+     * @return formatted string
+     */
+    public String display() {
+        return "X = " + x + ", Y = " + y;
+    }
+}
